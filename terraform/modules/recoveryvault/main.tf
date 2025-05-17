3dec6bfdf8af6d75cb76485ee5d98b7959dc294c
cloudcomputingconsortium <samuel.navarro@cloudcomputingconsortium.com>
2025-05-17 00:15:42 -0600

    Delete terraform directory

--- terraform/modules/recoveryvault/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_recovery_services_vault" "core" {
-  name                = "core-backupvault"
-  location            = azurerm_resource_group.core.location
-  resource_group_name = azurerm_resource_group.core.name
-  sku                 = "Standard"
-}
